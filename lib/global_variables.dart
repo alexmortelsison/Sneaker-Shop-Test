c1d109d5724b40add36b6f69548208988f3ecba1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-17 17:22:41 +0900

    updated UI

--- lib/global_variables.dart
+++ lib/global_variables.dart
@@ -1,8 +1,62 @@
 final products = [
   {
-    'title': 'Men\'s Shoes',
-    'price': 44.52,
+    'title': 'Jordan 1 HIGH OG "Satin Bred"',
+    'price': 172,
+    'sizes': [8, 9, 10, 11, 12],
+    'imageURL': 'images/1.png',
+    'company': 'Nike',
+  },
+  {
+    'title': 'Nike Air Max SYSTM',
+    'price': 76,
     'sizes': [8, 9, 10, 11],
-    'imageURL': ''
+    'imageURL': 'images/2.png',
+    'company': 'Nike',
+  },
+  {
+    'title': 'Nike V2K Run',
+    'price': 85,
+    'sizes': [8, 9, 10, 11],
+    'imageURL': 'images/3.png',
+    'company': 'Nike',
+  },
+  {
+    'title': 'Stan Smith x KSENIASCHNAIDER',
+    'price': 98,
+    'sizes': [8, 9, 10, 11],
+    'imageURL': 'images/4.png',
+    'company': 'Adidas',
+  },
+  {
+    'title': 'Campus 2.0 KORN',
+    'price': 120,
+    'sizes': [8, 9, 10, 11],
+    'imageURL': 'images/5.png',
+    'company': 'Adidas',
+  },
+  {
+    'title': 'Bad Bunny Last Campus',
+    'price': 155,
+    'sizes': [8, 9, 10, 11],
+    'imageURL': 'images/6.png',
+    'company': 'Adidas',
+  },
+  {
+    'title': 'Star Player 76 Premium Canvas',
+    'price': 69,
+    'sizes': [8, 9, 10, 11],
+    'imageURL': 'images/8.png',
+    'company': 'Converse',
+  },
+];
+
+final cart = [
+  {
+    'id': '1',
+    'title': 'Jordan 1 HIGH OG "Satin Bred"',
+    'price': 172,
+    'sizes': [8, 9, 10, 11, 12],
+    'imageURL': 'images/1.png',
+    'company': 'Nike',
   }
 ];
