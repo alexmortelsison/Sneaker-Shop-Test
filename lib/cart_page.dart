d36aed8c1f04f24662ecb31b9b9d0322d138896b
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-18 10:37:40 +0900

    updated UI

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,13 +1,41 @@
 import 'package:flutter/material.dart';
+import 'global_variables.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Cart Page'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cart'),
+      ),
+      body: ListView.builder(
+        itemCount: cart.length,
+        itemBuilder: (context, index) {
+          final cartItem = cart[index];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: const Color.fromARGB(255, 211, 211, 211),
+              radius: 30,
+              backgroundImage: AssetImage('${cartItem['imageURL']}'),
+            ),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.delete,
+                color: Color.fromARGB(255, 145, 41, 34),
+              ),
+            ),
+            title: Text(
+              '${cartItem['title']}',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            subtitle: Text(
+              'Size: ${cartItem['sizes']}',
+            ),
+          );
+        },
       ),
     );
   }
